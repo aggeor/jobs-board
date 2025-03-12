131d3b7b61321f4a6d64a3a4a35552207b2b9087
Aggelos Georgiadis <ag.gewr@gmail.com>
2025-03-12 13:34:50 +0200

    add styling

--- src/app/components/JobCard.tsx
+++ src/app/components/JobCard.tsx
@@ -7,20 +7,21 @@ interface Props{
 
 export default function JobCard({data}:Props){
     return (
-        <div className="flex items-center m-2 bg-gray-600 rounded-2xl">
+        <div className="flex items-center m-2 bg-linear-to-r from-gray-900 via-gray-800 to-gray-500 rounded-2xl">
             <img className=" w-16 h-16 m-2 max-w-full rounded-2xl " src={data.companyLogo} alt="jobImg" />
             <a href={data.url} target="_blank" rel="noopener noreferrer">
                 
-                <div className="">
+                <div className="font-normal text-base">
+                    {data.companyName}
+                </div>
+
+                <div className="font-bold text-lg">
                     {data.jobTitle}
                 </div>
                 
-                <div className="">
-                    {data.companyName} 
-                </div>
                 
-                <div className="">
-                    ({data.jobGeo})
+                <div className="font-light text-xs">
+                    {data.jobGeo} {data.jobType.map((type)=> type.toUpperCase())}
                 </div>
             </a>
         </div>
