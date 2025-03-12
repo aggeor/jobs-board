e13cd4cbaf7fa3d3938c5b8e6287f0a96f3401eb
Aggelos Georgiadis <ag.gewr@gmail.com>
2025-03-12 16:21:18 +0200

    add date

--- src/app/components/JobCard.tsx
+++ src/app/components/JobCard.tsx
@@ -4,30 +4,42 @@ import replaceAmp from "../helper";
 import Tag, { TagType } from "./Tag";
 import { FaLocationDot } from "react-icons/fa6";
 
-interface Props{
-    data: Job
+interface Props {
+    data: Job;
 }
 
-export default function JobCard({data}:Props){
+export default function JobCard({ data }: Props) {
+    const date = data.pubDate.toLocaleString();
     return (
-        <div className="flex items-center m-2 bg-linear-to-r from-gray-900 via-gray-800 to-gray-500 rounded-2xl">
-            <img className=" w-16 h-16 m-2 max-w-full rounded-2xl " src={data.companyLogo} alt="jobImg" />
-            <a href={data.url} target="_blank" rel="noopener noreferrer">
-                
-                <div className="font-normal text-base mt-3">
-                    {data.companyName} {data.jobIndustry.map((jobIndustry)=> <Tag key={data.id} type = { TagType.jobIndustry } text = { jobIndustry } />)}
-                </div>
+        <div className="flex items-center m-2 bg-linear-to-r from-gray-900 via-gray-800 to-gray-500 rounded-2xl p-4">
+            <img className="w-16 h-16 max-w-full rounded-2xl" src={data.companyLogo} alt="jobImg" />
+            
+            <div className="ml-3 flex flex-col w-full">
+                <a href={data.url} target="_blank" rel="noopener noreferrer">
+                    <div className="font-normal text-base">
+                        {data.companyName} {data.jobIndustry.map((jobIndustry) => (
+                            <Tag key={jobIndustry} type={TagType.jobIndustry} text={jobIndustry} />
+                        ))}
+                    </div>
 
-                <div className="font-bold text-lg">
-                    {replaceAmp(data.jobTitle)}
-                </div>
-                
-                <div className="font-light text-xs flex items-start mb-3 mt-1">
-                    <FaLocationDot className="mr-1" /> 
-                    <div className="mr-1">{data.jobGeo}</div>
-                    <div>{data.jobType.map((type)=> <Tag key={data.id} type = { TagType.jobType } text = { type } />)}</div>
-                </div>
-            </a>
+                    <div className="font-bold text-lg">
+                        {replaceAmp(data.jobTitle)}
+                    </div>
+
+                    <div className="font-light text-xs flex items-center justify-between mt-1">
+                        <div className="flex items-center">
+                            <FaLocationDot className="mr-1" />
+                            <div className="mr-2">{data.jobGeo}</div>
+                            <div>{data.jobType.map((type) => (
+                                <Tag key={type} type={TagType.jobType} text={type} />
+                            ))}</div>
+                        </div>
+                        <div className="font-light text-xs whitespace-nowrap">
+                            {date}
+                        </div>
+                    </div>
+                </a>
+            </div>
         </div>
     );
 }
