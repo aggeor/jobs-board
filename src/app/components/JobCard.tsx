0b4b0306f9c08f2d84410896da8d0e1f476db250
Aggelos Georgiadis <ag.gewr@gmail.com>
2025-03-12 15:04:02 +0200

    add location icon

--- src/app/components/JobCard.tsx
+++ src/app/components/JobCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Job, JobType } from "../api";
 import Tag, { TagType } from "./Tag";
+import { FaLocationDot } from "react-icons/fa6";
 
 interface Props{
     data: Job
@@ -12,7 +13,7 @@ export default function JobCard({data}:Props){
             <img className=" w-16 h-16 m-2 max-w-full rounded-2xl " src={data.companyLogo} alt="jobImg" />
             <a href={data.url} target="_blank" rel="noopener noreferrer">
                 
-                <div className="font-normal text-base">
+                <div className="font-normal text-base mt-3">
                     {data.companyName} {data.jobIndustry.map((jobIndustry)=> <Tag key={data.id} type = { TagType.jobIndustry } text = { jobIndustry } />)}
                 </div>
 
@@ -20,9 +21,10 @@ export default function JobCard({data}:Props){
                     {data.jobTitle}
                 </div>
                 
-                
-                <div className="font-light text-xs">
-                    {data.jobGeo}  {data.jobType.map((type)=> <Tag key={data.id} type = { TagType.jobType } text = { type } />)}
+                <div className="font-light text-xs flex items-start mb-3 mt-1">
+                    <FaLocationDot className="mr-1" /> 
+                    <div className="mr-1">{data.jobGeo}</div>
+                    <div>{data.jobType.map((type)=> <Tag key={data.id} type = { TagType.jobType } text = { type } />)}</div>
                 </div>
             </a>
         </div>
