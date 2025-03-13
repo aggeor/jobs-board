711ab1601fefcd125c874d14a8b2995a69f909a3
Aggelos Georgiadis <ag.gewr@gmail.com>
2025-03-13 15:25:27 +0200

    fix searchbar size

--- src/app/components/Searchbar.tsx
+++ src/app/components/Searchbar.tsx
@@ -1,4 +1,3 @@
-
 import { ChangeEvent, FC, FormEvent } from 'react'
 
 interface SearchBarProps {
@@ -15,19 +14,19 @@ export default function SearchBar ({
   onChange,
   onClear,
   placeholder = 'Search...',
-}:SearchBarProps) {
+}: SearchBarProps) {
   return (
     <form
       onSubmit={onSubmit}
-      className="mx-auto flex w-full max-w-3xl flex-col items-center gap-3 rounded-xl border-2 border-[#EBEEF7] bg-white px-3 py-2 md:flex-row"
+      className="mx-auto flex w-full max-w-md flex-col items-center gap-3 rounded-xl border-2 border-[#EBEEF7] bg-white px-2 py-1 md:flex-row"
     >
-      <div className="ml-4 flex w-full items-center gap-3">
+      <div className="ml-4 flex w-full items-center gap-2">
         <input
           name="search"
           value={value}
           onChange={onChange}
           placeholder={placeholder}
-          className="w-full py-4 text-[#939AAD] outline-none"
+          className="w-full py-2 text-[#939AAD] outline-none"
         />
         {value && (
           <button
@@ -42,7 +41,7 @@ export default function SearchBar ({
 
       <button
         type="submit"
-        className="flex w-full items-center justify-center gap-3 rounded-md bg-[#00AAFF] px-5 py-2.5 text-lg text-white transition duration-300 ease-in-out hover:bg-[#037AB6] focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 md:w-auto"
+        className="flex w-full items-center justify-center gap-2 rounded-md bg-[#00AAFF] px-4 py-1 text-lg text-white transition duration-300 ease-in-out hover:bg-[#037AB6] focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 md:w-auto"
       >
         Search
       </button>
